615d86daea14f1b194875a3247c5f3521b8c22f5
Said Rodrigues <said.cavalcante.rodrigues@sap.com>
2026-03-09 19:32:27 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import session from 'express-session';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +18,19 @@ async function bootstrap() {
     }),
   );
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('DC Warehouse POC example')
+    .setDescription('DC Warehouse cats API description')
+    .setVersion('0.420.69')
+    .addTag('warehouse')
+    .build();
+  const document = () => SwaggerModule.createDocument(app, config);
+  app.use(
+    '/reference',
+    apiReference({
+      content: document,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
